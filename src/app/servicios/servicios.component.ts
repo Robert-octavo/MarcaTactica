8088be98c875fbb646ef0fd9ec40b0d134cff1d9
Robert Ortega <robert.ortega.octavo@gmail.com>
2023-02-23 14:27:10 -0500

    update services page

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee, faChartBar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-servicios',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./servicios.component.css']
 })
 export class ServiciosComponent {
-
+  faCoffee = faCoffee;
+  faChartBar = faChartBar;
 }
